90fff87141e95c91ea5bfdde96995efb17e3f4be
V <44793625+LucianoAndrian@users.noreply.github.com>
2020-01-11 20:37:10 -0300

    funcion porcentaje y juego

--- Monty_Hall.R
+++ Monty_Hall.R
@@ -1,46 +1,87 @@
-corrects_results = vector(length = 100)
-for(j in 1:100){
-  
-  results = vector(length = 10000)
-  
-  for(i in 1:10000){
-    v = rnorm(3)
-    first_door = rnorm(3)
-    first_door = which(first_door == max(first_door))
+mh_per = function(n,m){
+  corrects_results = vector(length = n)
+  for(j in 1:n){
     
-    max_v = which(v == max(v))
+    results = vector(length = m)
     
-    empty_door = which(v != v[max_v] & v != v[first_door])
-    if(length(empty_door) == 2){
-      random = rnorm(2)
-      random = which(random == max(random))
-      empty_door = empty_door[random] 
-    } 
-    
-    #cambia de puerta? si
-    
-    change_door = which(v != v[empty_door] & v != v[first_door])
-    
-    
-    
-    if(v[change_door] == max(v)){
-      results[i] = 1
-    } else {
-      results[i] = 0 
+    for(i in 1:m){
+      v = rnorm(3)
+      first_door = rnorm(3)
+      first_door = which(first_door == max(first_door))
+      
+      max_v = which(v == max(v))
+      
+      empty_door = which(v != v[max_v] & v != v[first_door])
+      if(length(empty_door) == 2){
+        random = rnorm(2)
+        random = which(random == max(random))
+        empty_door = empty_door[random] 
+      } 
+      
+      #cambia de puerta? si
+      
+      change_door = which(v != v[empty_door] & v != v[first_door])
+      
+      
+      
+      if(v[change_door] == max(v)){
+        results[i] = 1
+      } else {
+        results[i] = 0 
+      }
+      
     }
+    
+    corrects = length(which(results == 1))
+    
+    
+    corrects_results[j] = corrects
+    
+    #print(paste("Correctos: ", as.character(correctos)))
+    
     
   }
   
-  corrects = length(which(results == 1))
+  porcentaje_acertado = mean(corrects_results)*100/m
+  
+  return(print(paste("Porcentaje acertado: ", as.character(round(porcentaje_acertado,digits = 2)), "%", sep = "")))
   
   
-  corrects_results[j] = corrects
   
-  #print(paste("Correctos: ", as.character(correctos)))
-  
+} 
 
+monty_hall = function(){
+  
+  first_door = as.numeric(readline("Elija una puerta [1] [2] [3]: "))
+  
+  while(first_door > 3){
+    first_door  = as.numeric(readline("Elija una puerta [1] [2] [3]: "))
+  }
+  
+  v = rnorm(3)
+  
+  max_v = which(v == max(v))
+  
+  empty_door = which(v != v[max_v] & v != v[first_door])
+  if(length(empty_door) == 2){
+    random = rnorm(2)
+    random = which(random == max(random))
+    empty_door = empty_door[random] 
+  }
+  
+  option_door = which(v != v[empty_door] & v != v[first_door])
+  
+  change = readline(paste("En la puerta ", as.character(empty_door), " no hay nada.", " Quiere cambiar su elección a la puerta ", as.character(option_door), "?: ", sep = ""))
+  while(change != "si" & change != "no"){
+    change = readline(paste("En la puerta ", as.character(empty_door), " no hay nada.", " Quiere cambiar su elección a la puerta ", as.character(option_door), "?: ", sep = ""))
+  }
+  if(change == "si"){
+    change_door = which(v != v[empty_door] & v != v[first_door])
+  }
+  
+  if(v[change_door] == v[max_v]){
+    print("correcto")
+  } else {
+    print("incorrecto")
+  }
 }
-
-porcentaje_acertado = mean(corrects_results)*100/10000
-
-print(paste("Porcentaje acertado: ", as.character(round(porcentaje_acertado,digits = 2)), "%", sep = ""))
